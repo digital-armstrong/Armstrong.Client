44ed5e86ffda1d787ef1ad2620d5e83d8689cebe
Alexander Veselov <collabsis.g@gmail.com>
2023-04-12 13:23:10 +0400

    Added redis for save channel info button

---
+++ Armstrong.Client/Helpers/RedisHelper.cs
@@ -0,0 +1,16 @@
+﻿using StackExchange.Redis;
+
+namespace Armstrong.Client.Helpers
+{
+    public class RedisHelper
+    {
+        public ConnectionMultiplexer RedisConnMultiplexer { get; set; }
+        public ISubscriber RedisSubscriber { get; set; }
+
+        public RedisHelper(string host)
+        {
+            RedisConnMultiplexer = ConnectionMultiplexer.Connect(host);
+            RedisSubscriber = RedisConnMultiplexer.GetSubscriber();
+        }
+    }
+}
